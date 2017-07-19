f8403f34251af59437519a0be9a21a1c1595376f
Dave Ackley <ackley@ackleyshack.com>
2017-07-19 05:43:51 +0000

    Try being modren with systemd

---
+++ files/opt/t2-startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run final boot time configurations for T2 pins
+
+logger STARTING T2 FINAL BOOT CONFIG
+
+logger T2 FINAL BOOT CONFIG: cape-universal2
+echo cape-universal2 > /sys/devices/platform/bone_capemgr/slots
+sleep 2
+
+logger T2 FINAL BOOT CONFIG: boot-config-pin
+config-pin -f /opt/scripts/t2/boot-config-pin.txt
+sleep 2
+
+logger T2 FINAL BOOT CONFIG: waveshare35a
+echo waveshare35a > /sys/devices/platform/bone_capemgr/slots
+sleep 2
+
+logger T2 FINAL BOOT CONFIG FINISHED
+
+exit 0
