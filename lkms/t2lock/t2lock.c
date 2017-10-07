e3af34bbc19c117ce417bfcd1327ab167df6d896
Dave Ackley <ackley@ackleyshack.com>
2017-10-07 06:38:24 -0600

    Switch to led lkm demo for base

--- lkms/t2lock/t2lock.c
+++ lkms/t2lock/t2lock.c
@@ -1,197 +1,180 @@
 /**
- * @file   t2lock.c
- * @author Dave Ackley
- * @date   6 October 2017
- * @version 0.1
- *
- * @brief A character driver to support the intertile locking
- * semantics of the MFM T2 tile. This module maps to /dev/t2/itlocks
- *
- * @see http://robust.cs.unm.edu/ for a backgrounder on the project,
- * and who-knows-where for a specific description of the T2 intertile
- * locking semantics.
+ * @file   led.c
+ * @author Derek Molloy
+ * @date   19 April 2015
+ * @brief  A kernel module for controlling a simple LED (or any signal) that is connected to
+ * a GPIO. It is threaded in order that it can flash the LED.
+ * The sysfs entry appears at /sys/ebb/led49
+ * @see http://www.derekmolloy.ie/
+*/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>       // Required for the GPIO functions
+#include <linux/kobject.h>    // Using kobjects for the sysfs bindings
+#include <linux/kthread.h>    // Using kthreads for the flashing functionality
+#include <linux/delay.h>      // Using this header for the msleep() function
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Derek Molloy");
+MODULE_DESCRIPTION("A simple Linux LED driver LKM for the BBB");
+MODULE_VERSION("0.1");
+
+static unsigned int gpioLED = 49;           ///< Default GPIO for the LED is 49
+module_param(gpioLED, uint, S_IRUGO);       ///< Param desc. S_IRUGO can be read/not changed
+MODULE_PARM_DESC(gpioLED, " GPIO LED number (default=49)");     ///< parameter description
+
+static unsigned int blinkPeriod = 1000;     ///< The blink period in ms
+module_param(blinkPeriod, uint, S_IRUGO);   ///< Param desc. S_IRUGO can be read/not changed
+MODULE_PARM_DESC(blinkPeriod, " LED blink period in ms (min=1, default=1000, max=10000)");
+
+static char ledName[7] = "ledXXX";          ///< Null terminated default string -- just in case
+static bool ledOn = 0;                      ///< Is the LED on or off? Used for flashing
+enum modes { OFF, ON, FLASH };              ///< The available LED modes -- static not useful here
+static enum modes mode = FLASH;             ///< Default mode is flashing
+
+/** @brief A callback function to display the LED mode
+ *  @param kobj represents a kernel object device that appears in the sysfs filesystem
+ *  @param attr the pointer to the kobj_attribute struct
+ *  @param buf the buffer to which to write the number of presses
+ *  @return return the number of characters of the mode string successfully displayed
  */
+static ssize_t mode_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
+   switch(mode){
+      case OFF:   return sprintf(buf, "off\n");       // Display the state -- simplistic approach
+      case ON:    return sprintf(buf, "on\n");
+      case FLASH: return sprintf(buf, "flash\n");
+      default:    return sprintf(buf, "LKM Error\n"); // Cannot get here
+   }
+}
 
-#include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h>         // Core header for loading LKMs into the kernel
-#include <linux/device.h>         // Header to support the kernel Driver Model
-#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
-#include <linux/fs.h>             // Header for the Linux file system support
-#include <asm/uaccess.h>          // Required for the copy to user function
-#include <linux/mutex.h>	  // Required for the mutex functionality
-#define  DEVICE_NAME "itlocks"    ///< The device will appear at /dev/t2/ using this value
-#define  CLASS_NAME  "t2"        ///< The device class -- this is a character device driver
+/** @brief A callback function to store the LED mode using the enum above */
+static ssize_t mode_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
+   // the count-1 is important as otherwise the \n is used in the comparison
+   if (strncmp(buf,"on",count-1)==0) { mode = ON; }   // strncmp() compare with fixed number chars
+   else if (strncmp(buf,"off",count-1)==0) { mode = OFF; }
+   else if (strncmp(buf,"flash",count-1)==0) { mode = FLASH; }
+   return count;
+}
 
-MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Dave Ackley");    ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("Access and control the T2 intertile lock system");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");            ///< A version number to inform users
+/** @brief A callback function to display the LED period */
+static ssize_t period_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
+   return sprintf(buf, "%d\n", blinkPeriod);
+}
 
-static int    majorNumber;                  ///< Store the device number -- determined automatically
-static char   message[256] = {0};           ///< Memory for the string that is passed from userspace
-static short  size_of_message;              ///< Used to remember the size of the string stored
-static int    numberOpens = 0;              ///< Counts the number of times the device is opened
-static struct class*  t2lockClass  = NULL; ///< The device-driver class struct pointer
-static struct device* t2lockDevice = NULL; ///< The device-driver device struct pointer
+/** @brief A callback function to store the LED period value */
+static ssize_t period_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
+   unsigned int period;                     // Using a variable to validate the data sent
+   sscanf(buf, "%du", &period);             // Read in the period as an unsigned int
+   if ((period>1)&&(period<=10000)){        // Must be 2ms or greater, 10secs or less
+      blinkPeriod = period;                 // Within range, assign to blinkPeriod variable
+   }
+   return period;
+}
 
-static DEFINE_MUTEX(t2lock_mutex);	    ///< Macro to declare a new mutex
-
-/// The prototype functions for the character driver -- must come before the struct definition
-static int     dev_open(struct inode *, struct file *);
-static int     dev_release(struct inode *, struct file *);
-static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
-static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
-
-/**
- * Devices are represented as file structure in the kernel. The file_operations structure from
- * /linux/fs.h lists the callback functions that you wish to associated with your file operations
- * using a C99 syntax structure. char devices usually implement open, read, write and release calls
+/** Use these helper macros to define the name and access levels of the kobj_attributes
+ *  The kobj_attribute has an attribute attr (name and mode), show and store function pointers
+ *  The period variable is associated with the blinkPeriod variable and it is to be exposed
+ *  with mode 0666 using the period_show and period_store functions above
  */
-static struct file_operations fops =
-{
-   .open = dev_open,
-   .read = dev_read,
-   .write = dev_write,
-   .release = dev_release,
+static struct kobj_attribute period_attr = __ATTR(blinkPeriod, 0666, period_show, period_store);
+static struct kobj_attribute mode_attr = __ATTR(mode, 0666, mode_show, mode_store);
+
+/** The ebb_attrs[] is an array of attributes that is used to create the attribute group below.
+ *  The attr property of the kobj_attribute is used to extract the attribute struct
+ */
+static struct attribute *ebb_attrs[] = {
+   &period_attr.attr,                       // The period at which the LED flashes
+   &mode_attr.attr,                         // Is the LED on or off?
+   NULL,
 };
+
+/** The attribute group uses the attribute array and a name, which is exposed on sysfs -- in this
+ *  case it is gpio49, which is automatically defined in the ebbLED_init() function below
+ *  using the custom kernel parameter that can be passed when the module is loaded.
+ */
+static struct attribute_group attr_group = {
+   .name  = ledName,                        // The name is generated in ebbLED_init()
+   .attrs = ebb_attrs,                      // The attributes array defined just above
+};
+
+static struct kobject *ebb_kobj;            /// The pointer to the kobject
+static struct task_struct *task;            /// The pointer to the thread task
+
+/** @brief The LED Flasher main kthread loop
+ *
+ *  @param arg A void pointer used in order to pass data to the thread
+ *  @return returns 0 if successful
+ */
+static int flash(void *arg){
+   printk(KERN_INFO "EBB LED: Thread has started running \n");
+   while(!kthread_should_stop()){           // Returns true when kthread_stop() is called
+      set_current_state(TASK_RUNNING);
+      if (mode==FLASH) ledOn = !ledOn;      // Invert the LED state
+      else if (mode==ON) ledOn = true;
+      else ledOn = false;
+      gpio_set_value(gpioLED, ledOn);       // Use the LED state to light/turn off the LED
+      set_current_state(TASK_INTERRUPTIBLE);
+      msleep(blinkPeriod/2);                // millisecond sleep for half of the period
+   }
+   printk(KERN_INFO "EBB LED: Thread has run to completion \n");
+   return 0;
+}
 
 /** @brief The LKM initialization function
  *  The static keyword restricts the visibility of the function to within this C file. The __init
  *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
- *  time and that it can be discarded and its memory freed up after that point.
+ *  time and that it can be discarded and its memory freed up after that point. In this example this
+ *  function sets up the GPIOs and the IRQ
  *  @return returns 0 if successful
  */
-static int __init t2lock_init(void){
-   printk(KERN_INFO "T2lock: Initializing t2lock LKM\n");
+static int __init ebbLED_init(void){
+   int result = 0;
 
-   // Try to dynamically allocate a major number for the device -- more difficult but worth it
-   majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-   if (majorNumber<0){
-      printk(KERN_ALERT "T2lock failed to register a major number\n");
-      return majorNumber;
-   }
-   printk(KERN_INFO "T2lock: registered correctly with major number %d\n", majorNumber);
+   printk(KERN_INFO "EBB LED: Initializing the EBB LED LKM\n");
+   sprintf(ledName, "led%d", gpioLED);      // Create the gpio115 name for /sys/ebb/led49
 
-   // Register the device class
-   t2lockClass = class_create(THIS_MODULE, CLASS_NAME);
-   if (IS_ERR(t2lockClass)){           // Check for error and clean up if there is
-      unregister_chrdev(majorNumber, DEVICE_NAME);
-      printk(KERN_ALERT "Failed to register device class\n");
-      return PTR_ERR(t2lockClass);     // Correct way to return an error on a pointer
+   ebb_kobj = kobject_create_and_add("ebb", kernel_kobj->parent); // kernel_kobj points to /sys/kernel
+   if(!ebb_kobj){
+      printk(KERN_ALERT "EBB LED: failed to create kobject\n");
+      return -ENOMEM;
    }
-   printk(KERN_INFO "T2lock: device class registered correctly\n");
+   // add the attributes to /sys/ebb/ -- for example, /sys/ebb/led49/ledOn
+   result = sysfs_create_group(ebb_kobj, &attr_group);
+   if(result) {
+      printk(KERN_ALERT "EBB LED: failed to create sysfs group\n");
+      kobject_put(ebb_kobj);                // clean up -- remove the kobject sysfs entry
+      return result;
+   }
+   ledOn = true;
+   gpio_request(gpioLED, "sysfs");          // gpioLED is 49 by default, request it
+   gpio_direction_output(gpioLED, ledOn);   // Set the gpio to be in output mode and turn on
+   gpio_export(gpioLED, false);  // causes gpio49 to appear in /sys/class/gpio
+                                 // the second argument prevents the direction from being changed
 
-   // Register the device driver
-   t2lockDevice = device_create(t2lockClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-   if (IS_ERR(t2lockDevice)){          // Clean up if there is an error
-      class_destroy(t2lockClass);      // Repeated code but the alternative is goto statements
-      unregister_chrdev(majorNumber, DEVICE_NAME);
-      printk(KERN_ALERT "Failed to create the device\n");
-      return PTR_ERR(t2lockDevice);
+   task = kthread_run(flash, NULL, "LED_flash_thread");  // Start the LED flashing thread
+   if(IS_ERR(task)){                                     // Kthread name is LED_flash_thread
+      printk(KERN_ALERT "EBB LED: failed to create the task\n");
+      return PTR_ERR(task);
    }
-   printk(KERN_INFO "T2lock: device class created correctly\n"); // Made it! device was initialized
-   mutex_init(&t2lock_mutex);          // Initialize the mutex dynamically
-   return 0;
+   return result;
 }
 
 /** @brief The LKM cleanup function
  *  Similar to the initialization function, it is static. The __exit macro notifies that if this
  *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
-static void __exit t2lock_exit(void){
-   mutex_destroy(&t2lock_mutex);                       // destroy the dynamically-allocated mutex
-   device_destroy(t2lockClass, MKDEV(majorNumber, 0)); // remove the device
-   class_unregister(t2lockClass);                      // unregister the device class
-   class_destroy(t2lockClass);                         // remove the device class
-   unregister_chrdev(majorNumber, DEVICE_NAME);         // unregister the major number
-   printk(KERN_INFO "T2lock: Bye for now.\n");
+static void __exit ebbLED_exit(void){
+   kthread_stop(task);                      // Stop the LED flashing thread
+   kobject_put(ebb_kobj);                   // clean up -- remove the kobject sysfs entry
+   gpio_set_value(gpioLED, 0);              // Turn the LED off, indicates device was unloaded
+   gpio_unexport(gpioLED);                  // Unexport the Button GPIO
+   gpio_free(gpioLED);                      // Free the LED GPIO
+   printk(KERN_INFO "EBB LED: Goodbye from the EBB LED LKM!\n");
 }
 
-/** @brief The device open function that is called each time the device is opened
- *  This will only increment the numberOpens counter in this case.
- *  @param inodep A pointer to an inode object (defined in linux/fs.h)
- *  @param filep A pointer to a file object (defined in linux/fs.h)
- */
-static int dev_open(struct inode *inodep, struct file *filep){
-
-   if(!mutex_trylock(&t2lock_mutex)){                  // Try to acquire the mutex (returns 0 on fail)
-	printk(KERN_ALERT "T2lock: Device in use by another process");
-	return -EBUSY;
-   }
-   numberOpens++;
-   printk(KERN_INFO "T2lock: Device has been opened %d time(s)\n", numberOpens);
-   return 0;
-}
-
-/** @brief This function is called whenever device is being read from user space i.e. data is
- *  being sent from the device to the user. In this case is uses the copy_to_user() function to
- *  send the buffer string to the user and captures any errors.
- *  @param filep A pointer to a file object (defined in linux/fs.h)
- *  @param buffer The pointer to the buffer to which this function writes the data
- *  @param len The length of the b
- *  @param offset The offset if required
- */
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
-   int error_count = 0;
-   // copy_to_user has the format ( * to, *from, size) and returns 0 on success
-   error_count = copy_to_user(buffer, message, size_of_message);
-
-   if (error_count==0){           // success!
-      printk(KERN_INFO "T2lock: Sent %d characters to the user\n", size_of_message);
-      return (size_of_message=0); // clear the position to the start and return 0
-   }
-   else {
-      printk(KERN_INFO "T2lock: Failed to send %d characters to the user\n", error_count);
-      return -EFAULT;      // Failed -- return a bad address message (i.e. -14)
-   }
-}
-
-/** @brief This function is called whenever the device is being written to from user space i.e.
- *  data is sent to the device from the user. The data is copied to the message[] array in this
- *  LKM using message[x] = buffer[x]
- *  @param filep A pointer to a file object
- *  @param buffer The buffer to that contains the string to write to the device
- *  @param len The length of the array of data that is being passed in the const char buffer
- *  @param offset The offset if required
- */
-static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-
-  const size_t MAXLEN=100;
-  size_t bytesToCopy = (len>=MAXLEN) ? MAXLEN : len;
-  size_t bytesNotCopied, bytesCopied;
-  printk(KERN_INFO "T2lock: b2Copy %zu\n", bytesToCopy);
-
-  bytesNotCopied = copy_from_user(message, buffer, bytesToCopy);
-  printk(KERN_INFO "T2lock: b!Copied %zu\n", bytesNotCopied);
-
-  bytesCopied = bytesToCopy - bytesNotCopied;
-
-  printk(KERN_INFO "T2lock: bCopied %zu\n", bytesCopied);
-
-  sprintf(message + bytesCopied, "(%zu bytes)", bytesCopied);
-  size_of_message = strlen(message);     // store the length of the stored message
-   printk(KERN_INFO "T2lock: Received %zu characters from the user\n", bytesCopied);
-   if (bytesNotCopied) {
-     printk(KERN_INFO "T2lock: Failed to receive %zu bytes!\n", bytesNotCopied);
-     return -EFAULT;
-   }
-   return len;
-}
-
-/** @brief The device release function that is called whenever the device is closed/released by
- *  the userspace program
- *  @param inodep A pointer to an inode object (defined in linux/fs.h)
- *  @param filep A pointer to a file object (defined in linux/fs.h)
- */
-static int dev_release(struct inode *inodep, struct file *filep){
-   mutex_unlock(&t2lock_mutex);                      // release the mutex (i.e., lock goes up)
-   printk(KERN_INFO "T2lock: Device successfully closed\n");
-   return 0;
-}
-
-/** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
- *  identify the initialization function at insertion time and the cleanup function (as
- *  listed above)
- */
-module_init(t2lock_init);
-module_exit(t2lock_exit);
+/// This next calls are  mandatory -- they identify the initialization function
+/// and the cleanup function (as above).
+module_init(ebbLED_init);
+module_exit(ebbLED_exit);
---
+++ lkms/t2lock/t2lock10.c
@@ -0,0 +1,197 @@
+/**
+ * @file   t2lock.c
+ * @author Dave Ackley
+ * @date   6 October 2017
+ * @version 0.1
+ *
+ * @brief A character driver to support the intertile locking
+ * semantics of the MFM T2 tile. This module maps to /dev/t2/itlocks
+ *
+ * @see http://robust.cs.unm.edu/ for a backgrounder on the project,
+ * and who-knows-where for a specific description of the T2 intertile
+ * locking semantics.
+ */
+
+#include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
+#include <linux/module.h>         // Core header for loading LKMs into the kernel
+#include <linux/device.h>         // Header to support the kernel Driver Model
+#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
+#include <linux/fs.h>             // Header for the Linux file system support
+#include <asm/uaccess.h>          // Required for the copy to user function
+#include <linux/mutex.h>	  // Required for the mutex functionality
+#define  DEVICE_NAME "itlocks"    ///< The device will appear at /dev/t2/ using this value
+#define  CLASS_NAME  "t2"        ///< The device class -- this is a character device driver
+
+MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
+MODULE_AUTHOR("Dave Ackley");    ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("Access and control the T2 intertile lock system");  ///< The description -- see modinfo
+MODULE_VERSION("0.1");            ///< A version number to inform users
+
+static int    majorNumber;                  ///< Store the device number -- determined automatically
+static char   message[256] = {0};           ///< Memory for the string that is passed from userspace
+static short  size_of_message;              ///< Used to remember the size of the string stored
+static int    numberOpens = 0;              ///< Counts the number of times the device is opened
+static struct class*  t2lockClass  = NULL; ///< The device-driver class struct pointer
+static struct device* t2lockDevice = NULL; ///< The device-driver device struct pointer
+
+static DEFINE_MUTEX(t2lock_mutex);	    ///< Macro to declare a new mutex
+
+/// The prototype functions for the character driver -- must come before the struct definition
+static int     dev_open(struct inode *, struct file *);
+static int     dev_release(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+
+/**
+ * Devices are represented as file structure in the kernel. The file_operations structure from
+ * /linux/fs.h lists the callback functions that you wish to associated with your file operations
+ * using a C99 syntax structure. char devices usually implement open, read, write and release calls
+ */
+static struct file_operations fops =
+{
+   .open = dev_open,
+   .read = dev_read,
+   .write = dev_write,
+   .release = dev_release,
+};
+
+/** @brief The LKM initialization function
+ *  The static keyword restricts the visibility of the function to within this C file. The __init
+ *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
+ *  time and that it can be discarded and its memory freed up after that point.
+ *  @return returns 0 if successful
+ */
+static int __init t2lock_init(void){
+   printk(KERN_INFO "T2lock: Initializing t2lock LKM\n");
+
+   // Try to dynamically allocate a major number for the device -- more difficult but worth it
+   majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+   if (majorNumber<0){
+      printk(KERN_ALERT "T2lock failed to register a major number\n");
+      return majorNumber;
+   }
+   printk(KERN_INFO "T2lock: registered correctly with major number %d\n", majorNumber);
+
+   // Register the device class
+   t2lockClass = class_create(THIS_MODULE, CLASS_NAME);
+   if (IS_ERR(t2lockClass)){           // Check for error and clean up if there is
+      unregister_chrdev(majorNumber, DEVICE_NAME);
+      printk(KERN_ALERT "Failed to register device class\n");
+      return PTR_ERR(t2lockClass);     // Correct way to return an error on a pointer
+   }
+   printk(KERN_INFO "T2lock: device class registered correctly\n");
+
+   // Register the device driver
+   t2lockDevice = device_create(t2lockClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+   if (IS_ERR(t2lockDevice)){          // Clean up if there is an error
+      class_destroy(t2lockClass);      // Repeated code but the alternative is goto statements
+      unregister_chrdev(majorNumber, DEVICE_NAME);
+      printk(KERN_ALERT "Failed to create the device\n");
+      return PTR_ERR(t2lockDevice);
+   }
+   printk(KERN_INFO "T2lock: device class created correctly\n"); // Made it! device was initialized
+   mutex_init(&t2lock_mutex);          // Initialize the mutex dynamically
+   return 0;
+}
+
+/** @brief The LKM cleanup function
+ *  Similar to the initialization function, it is static. The __exit macro notifies that if this
+ *  code is used for a built-in driver (not a LKM) that this function is not required.
+ */
+static void __exit t2lock_exit(void){
+   mutex_destroy(&t2lock_mutex);                       // destroy the dynamically-allocated mutex
+   device_destroy(t2lockClass, MKDEV(majorNumber, 0)); // remove the device
+   class_unregister(t2lockClass);                      // unregister the device class
+   class_destroy(t2lockClass);                         // remove the device class
+   unregister_chrdev(majorNumber, DEVICE_NAME);         // unregister the major number
+   printk(KERN_INFO "T2lock: Bye for now.\n");
+}
+
+/** @brief The device open function that is called each time the device is opened
+ *  This will only increment the numberOpens counter in this case.
+ *  @param inodep A pointer to an inode object (defined in linux/fs.h)
+ *  @param filep A pointer to a file object (defined in linux/fs.h)
+ */
+static int dev_open(struct inode *inodep, struct file *filep){
+
+   if(!mutex_trylock(&t2lock_mutex)){                  // Try to acquire the mutex (returns 0 on fail)
+	printk(KERN_ALERT "T2lock: Device in use by another process");
+	return -EBUSY;
+   }
+   numberOpens++;
+   printk(KERN_INFO "T2lock: Device has been opened %d time(s)\n", numberOpens);
+   return 0;
+}
+
+/** @brief This function is called whenever device is being read from user space i.e. data is
+ *  being sent from the device to the user. In this case is uses the copy_to_user() function to
+ *  send the buffer string to the user and captures any errors.
+ *  @param filep A pointer to a file object (defined in linux/fs.h)
+ *  @param buffer The pointer to the buffer to which this function writes the data
+ *  @param len The length of the b
+ *  @param offset The offset if required
+ */
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+   int error_count = 0;
+   // copy_to_user has the format ( * to, *from, size) and returns 0 on success
+   error_count = copy_to_user(buffer, message, size_of_message);
+
+   if (error_count==0){           // success!
+      printk(KERN_INFO "T2lock: Sent %d characters to the user\n", size_of_message);
+      return (size_of_message=0); // clear the position to the start and return 0
+   }
+   else {
+      printk(KERN_INFO "T2lock: Failed to send %d characters to the user\n", error_count);
+      return -EFAULT;      // Failed -- return a bad address message (i.e. -14)
+   }
+}
+
+/** @brief This function is called whenever the device is being written to from user space i.e.
+ *  data is sent to the device from the user. The data is copied to the message[] array in this
+ *  LKM using message[x] = buffer[x]
+ *  @param filep A pointer to a file object
+ *  @param buffer The buffer to that contains the string to write to the device
+ *  @param len The length of the array of data that is being passed in the const char buffer
+ *  @param offset The offset if required
+ */
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
+
+  const size_t MAXLEN=100;
+  size_t bytesToCopy = (len>=MAXLEN) ? MAXLEN : len;
+  size_t bytesNotCopied, bytesCopied;
+  printk(KERN_INFO "T2lock: b2Copy %zu\n", bytesToCopy);
+
+  bytesNotCopied = copy_from_user(message, buffer, bytesToCopy);
+  printk(KERN_INFO "T2lock: b!Copied %zu\n", bytesNotCopied);
+
+  bytesCopied = bytesToCopy - bytesNotCopied;
+
+  printk(KERN_INFO "T2lock: bCopied %zu\n", bytesCopied);
+
+  sprintf(message + bytesCopied, "(%zu bytes)", bytesCopied);
+  size_of_message = strlen(message);     // store the length of the stored message
+   printk(KERN_INFO "T2lock: Received %zu characters from the user\n", bytesCopied);
+   if (bytesNotCopied) {
+     printk(KERN_INFO "T2lock: Failed to receive %zu bytes!\n", bytesNotCopied);
+     return -EFAULT;
+   }
+   return len;
+}
+
+/** @brief The device release function that is called whenever the device is closed/released by
+ *  the userspace program
+ *  @param inodep A pointer to an inode object (defined in linux/fs.h)
+ *  @param filep A pointer to a file object (defined in linux/fs.h)
+ */
+static int dev_release(struct inode *inodep, struct file *filep){
+   mutex_unlock(&t2lock_mutex);                      // release the mutex (i.e., lock goes up)
+   printk(KERN_INFO "T2lock: Device successfully closed\n");
+   return 0;
+}
+
+/** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
+ *  identify the initialization function at insertion time and the cleanup function (as
+ *  listed above)
+ */
+module_init(t2lock_init);
+module_exit(t2lock_exit);
