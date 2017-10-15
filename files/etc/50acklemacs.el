f281895088b0017158ea5e35b5f7fc997e0c896d
Dave Ackley <ackley@ackleyshack.com>
2017-10-15 21:35:44 +0000

    Guts of dired-sort-map

--- files/etc/50acklemacs.el
+++ files/etc/50acklemacs.el
@@ -14,6 +14,22 @@
 
 (setq text-mode-hook '(lambda () (auto-fill-mode 1))) ; fill text files by default
 
+
+;;;;;;;
+;;; dired-sort-map.el --- in Dired: press s then s, x, t or n to sort by Size, eXtension, Time or Name
+;; Copyright (C) 2002 by Free Software Foundation, Inc.
+;; Author: Patrick Anderson
+
+(defvar dired-sort-map (make-sparse-keymap))
+
+(add-hook 'dired-mode-hook '(lambda () (define-key dired-mode-map "s" dired-sort-map)))
+(add-hook 'dired-mode-hook '(lambda () (define-key dired-sort-map "s" '(lambda () "sort by Size" (interactive) (dired-sort-other (concat dired-listing-switches "S"))))))
+(add-hook 'dired-mode-hook '(lambda () (define-key dired-sort-map "x" '(lambda () "sort by eXtension" (interactive) (dired-sort-other (concat dired-listing-switches "X"))))))
+(add-hook 'dired-mode-hook '(lambda () (define-key dired-sort-map "t" '(lambda () "sort by Time" (interactive) (dired-sort-other (concat dired-listing-switches "t"))))))
+(add-hook 'dired-mode-hook '(lambda () (define-key dired-sort-map "n" '(lambda () "sort by Name" (interactive) (dired-sort-other (concat dired-listing-switches ""))))))
+(provide 'dired-sort-map)
+;;;;;;;
+
 ;;; ^T
 (defun dha-ctl-t ()
   (interactive)
