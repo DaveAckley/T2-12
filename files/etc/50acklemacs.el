468b6ab1285013be744f1a015271e1da8a556b06
Dave Ackley <ackley@ackleyshack.com>
2017-11-12 14:18:07 +0000

    Add core go.el macros

--- files/etc/50acklemacs.el
+++ files/etc/50acklemacs.el
@@ -101,3 +101,150 @@
 (global-set-key "\C-xt" 'auto-fill-mode)    ; toggle auto fill
 (global-set-key "\C-x*" 'shell) ; start a sub shell (v19: not cmushell)
 
+;;;;;;;
+;;; go.el
+;; Written in prehistory by Dave when he didn't know emacs lisp for squat
+
+(defconst quarry-current-level nil)
+;; Sadly, mode-line-format won't display numbers.
+(defconst quarry-level-string nil)
+(defconst quarry-generation-string 0)
+
+(defvar quarry-source-buffer nil)
+
+(defvar quarry-map nil "Keybindings for quarry window.")
+
+(or nil; quarry-map DEBUG
+    (progn
+      (setq quarry-map (make-keymap))
+      (define-key quarry-map " " 'quarry-forward)
+      (define-key quarry-map "n" 'quarry-next)
+      (define-key quarry-map "b" 'quarry-prev)
+      (define-key global-map "\C-cs" 'quarry-start)
+      (define-key global-map "\C-cv" 'quarry-display-start)
+      (define-key global-map "\C-cd" 'timestamp-to-buffer)
+      (define-key global-map "\C-c\C-d" 'timestamp1-to-buffer)
+      (define-key global-map "\C-c;" 'quarry-comment-notestamp)
+      (define-key global-map "\C-cg" 'quarry-go)
+      (define-key global-map "\C-cs" 'quarry-start)
+      ))
+
+; Timestamp function
+(defun timestamp-to-buffer ()
+  (interactive)
+  (insert (current-time-string) " "))
+
+(defun timestamp1-to-buffer ()
+  (interactive)
+  (insert (format-time-string "%Y%m%d%H%M ")))
+
+
+(defun quarry-comment-notestamp ()
+    "Insert /* 61/NOTES:87 */ or the like, getting a new number, leaving us
+in the NOTES buffer"
+    (interactive)
+    (if (equal quarry-NOTES-buffer "")
+        (setq quarry-NOTES-buffer (read-from-minibuffer "NOTES buffer name: " "NOTES")))
+    (let ((nbuff (get-buffer quarry-NOTES-buffer)) bname bpoint btag qlevel)
+      (if (null nbuff) (error "No NOTES buffer"))
+      (setq bname (buffer-file-name nbuff))
+      (if (null bname) (error "NOTES has no associated file"))
+      (setq btag (quarry-file-last-dir bname))
+      (save-excursion
+        (set-buffer nbuff)
+        (quarry-init-if-necessary)
+        (setq qlevel (quarry-read-num))
+        (goto-char (point-max))
+        (quarry-go)
+        (insert "\n\n\n")
+        (backward-char 1)
+        (timestamp-to-buffer))
+      (indent-for-comment)
+      (insert btag ":" (format "%d " qlevel))
+      (my-bury-current-buffer)
+      (switch-to-buffer nbuff)));61/NOTES:4
+
+(defun quarry-go ()
+  "[quarry-step++ ]"
+  (interactive)
+  (quarry-init-if-necessary)
+  (let ((buf (current-buffer))
+        (num (quarry-read-num))
+        (atendp (>= (point) (point-max)))
+        (foo nil))
+
+    (set-buffer buf)
+    (insert "[")
+    (insert-string num)
+    (insert ": ")
+    (setq foo (point))
+    (insert "  :")
+    (insert-string num)
+    (insert "]")
+    (and atendp (insert "\n")); Fri Feb 21 10:10:45 1997 try to avoid leaving
+                                        ; lines w/o newlines at EOF (to make diff/patch happy)
+    (goto-char foo)
+    (quarry-increment-num)))
+
+(defun quarry-read-num ()
+  "extract current level number"
+  (interactive)
+  (save-excursion
+    (goto-char 2)
+    (mark-word 1)
+    (string-to-int (buffer-substring (point) (mark)))))
+
+(defun quarry-kill-num ()
+  "delete current level number"
+  (interactive)
+  (save-excursion
+    (goto-char 2)
+    (kill-word 1)))
+
+(defun quarry-replace-num (num)
+  "set current level to num"
+  (interactive)
+  (save-excursion
+    (quarry-kill-num)
+    (goto-char 2)
+    (insert-string num)))
+
+(defun quarry-increment-num ()
+  "increment and return level number"
+  (interactive)
+  (save-excursion
+    (let ((num (quarry-read-num)))
+      (setq num (+ num 1))
+      (quarry-replace-num num)
+      num)))
+
+(defun quarry-check-type ()
+  "T if this buffer inited for quarry"
+  (interactive)
+  (and (not (= (point-min) (point-max)))
+       (string-equal (buffer-substring 1 2) "{")))
+
+(defun quarry-init-if-necessary ()
+  "inited if not inited"
+  (interactive)
+  (if (quarry-check-type)
+      nil
+    (progn (quarry-init)
+           (goto-char (point-max)))))
+
+(defun quarry-start ()
+  "start or restart"
+  (interactive)
+  (save-excursion
+    (if (quarry-check-type)
+        (progn
+          (goto-char 1)
+          (kill-line 1)))
+    (quarry-init)))
+
+(defun quarry-init ()
+  "once per quarry file"
+  (interactive)
+  (goto-char 1)
+  (insert "{0}  -*- text -*- \n")
+  (text-mode))
