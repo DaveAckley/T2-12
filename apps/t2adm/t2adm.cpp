4c35297aac597de62c1c5f60b797735610243ff4
Dave Ackley <ackley@ackleyshack.com>
2017-11-14 16:00:39 +0000

    Refresh slower to reduce load

--- apps/t2adm/t2adm.cpp
+++ apps/t2adm/t2adm.cpp
@@ -80,7 +80,7 @@ int main() {
     ls.DrawITCStatus();
     hnc.MvPrintW(0,0,"");
     hnc.Refresh();
-    hnc.NapMS(1000/100); // 10 FPS
+    hnc.NapMS(1000/2); // 2 FPS
   }
   hnc.Shutdown();
   return 1;
