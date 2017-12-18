097bda179c8c034163ef31647c3119687b386346
Dave Ackley <ackley@ackleyshack.com>
2017-12-18 09:36:02 -0700

    Thread redesign and initialization

--- pru/itcio/firmware/prux_sbst3.asm
+++ pru/itcio/firmware/prux_sbst3.asm
@@ -12,21 +12,36 @@ STATE_INFO_LEN: .endstruct
 	
 LiveCounts:     .sassign R5, STATE_INFO
         
+;;; ThreadCoord: A length (in bytes) and an offset (in registers)
+ThreadCoord:      .struct
+bOffsetRegs:    .ubyte      ; offset in registers  (to load in r0.b0)
+bLenBytes:      .ubyte      ; length in bytes      (to load in r0.b1)
+ThreadCoord_LEN:  .endstruct
+
+;;; ThreadLoc: A union of a ThreadCoord and a word to load
+ThreadLoc:      .union
+sTC:    .tag ThreadCoord
+w:      .ushort                     
+        .endunion
+
+;;; ThreadHeader: Enough info to save, load, and context switch a thread
+ThreadHeader:   .struct
+sTHIS:          .tag ThreadLoc  ; Our thread storage location
+sNEXT:          .tag ThreadLoc  ; Next guy's storage location
+	
+bID:            .ubyte  ; thread id (prudir for 0..2, 3 for linux)
+bFLAGS:         .ubyte  ; flags
+wRES_ADDR:      .ushort ; Resume address after context switch	
+ThreadHeader_LEN:       .endstruct
+
 ;;; IOThread: Everything needed for a prudir state machine
 IOThread:       .struct
-bTHIS_ID:       .ubyte   ; this context id (0..2)
-bNEXT_ID:       .ubyte   ; next context id (0..2)
-bPREV_ID:       .ubyte   ; prev context id (0..2)
-bFLAGS:         .ubyte   ; flags
+sTH:            .tag ThreadHeader ; sTH takes two regs
 
 bTXRDY_PN:      .ubyte  ; Transmit Ready R30 Pin Number
 bTXDAT_PN:      .ubyte  ; Transmit Data  R30 Pin Number
 bRXRDY_PN:      .ubyte  ; Receive Ready  R31 Pin Number
 bRXDAT_PN:      .ubyte  ; Receive Data   R31 Pin Number
-
-wRES_ADDR:      .ushort ; Resume address after context switch
-bOUT1CNT:       .ubyte  ; current count of output 1s sent
-bINP1CNT:       .ubyte  ; current count of input 1s received
 
 bOUTDATA:       .ubyte  ; current bits being shifted out
 bOUTBCNT:       .ubyte  ; count of bits remaining to shift out
@@ -35,20 +50,40 @@ bINPBCNT:       .ubyte  ; count of bits remaining to shift in
 
 bPREVOUT:       .ubyte  ; last bit sent 
 bTHISOUT:       .ubyte  ; current bit to sent
-wRSRV2:         .ushort ; reserved
+bOUT1CNT:       .ubyte  ; current count of output 1s sent
+bINP1CNT:       .ubyte  ; current count of input 1s received
 
 rTXDAT_MASK:    .uint   ; 1<<TXDAT_PN
 rRSRV3:         .uint   ; reserved
 rRSRV4:         .uint   ; reserved
 IOThread_LEN:   .endstruct
         
+;;; LinuxThread: Everything needed for packet processing
+LinuxThread:    .struct        
+sTH:            .tag ThreadHeader ; sTH takes two regs
+LinuxThread_LEN: .endstruct     
+
 ;;; CT is the Current Thread!  It lives in R6-R13!
+	.eval IOThread_LEN/4, CTRegs
+	.eval 6, CTBaseReg
 	.asg R6, CTReg
 CT:     .sassign CTReg, IOThread
 
 	;int sendVal(const char * str, uint32_t val)
 	.ref sendVal            
-		
+
+        
+;;;;;;;;
+;;;: macro SENDVAL: Print two strings and a value
+;;;  INPUTS:
+;;;    STR1: First string to print
+;;;    STR2: Second string to print
+;;;    VAL: Value to print (reg or imm)
+;;;  OUTPUTS: None
+;;;  NOTES:
+;;;  - WARNING: MUST NOT BE USED IN LEAF FUNCTIONS!
+;;;  - TRASHES CALLER-SAVE REGS
+
 SENDVAL:        .macro STR1, STR2, REGVAL
 	.sect ".rodata:.string"
 $M1?:  .cstring STR1
@@ -64,17 +99,88 @@ $M2?:  .cstring STR2
         ADD R2, R2, 2           ; Pop stack
         .endm
 
+;;;;;;;;;
+;;;: macro LOADBIT: Copy SRCREG bit BITNUM to bottom of DESTREG
+;;;  INPUTS:
+;;;    SRCREG: Source register field; REG
+;;;    BITNUM: Number of bit (0 == LSB) to copy; OP(31)
+;;;    DSTREG: Destination register field; REG
+;;;  OUTPUTS:
+;;;    DESTREG is cleared except its bottom bit is SRCREG[BITNUM]
 LOADBIT:        .macro DESTREG, SRCREG, BITNUM
         LSR DESTREG, SRCREG, BITNUM ; Position desired bit at bottom of destreg
         AND DESTREG, DESTREG, 1     ; Flush the rest
         .endm
 
-	.text
+INITTHIS: .macro THISSHIFT,THISBYTES,ID,RESUMEADDR
+        ZERO &CT,IOThread_LEN                      ; Clear CT to start
+        LDI CT.sTH.sTHIS.sTC.bOffsetRegs, THISSHIFT
+        LDI CT.sTH.sTHIS.sTC.bLenBytes, THISBYTES
+        LDI CT.sTH.bID, ID
+        LDI CT.sTH.bFLAGS, 0
+	LDI CT.sTH.wRES_ADDR,RESUMEADDR
+	.endm
+INITNEXT:  .macro NEXTSHIFT,NEXTBYTES
+	LDI CT.sTH.sNEXT.sTC.bOffsetRegs, NEXTSHIFT
+        LDI CT.sTH.sNEXT.sTC.bLenBytes, NEXTBYTES
+        .endm
+
+SAVETHISCT: .macro
+        mov r0.w0, CT.sTH.sTHIS.w  ; r0.b0 <- this bOffsetRegs, r0.b1 <- this bLenBytes
+        xout PRUX_SCRATCH, &CT, b1 ; store this thread
+        .endm
+
+LOADNEXTCT: .macro
+        mov r0.w0, CT.sTH.sNEXT.w ; r0.b0 <- next bOffsetRegs, r0.b1 <- next bLenBytes
+        xin PRUX_SCRATCH, &CT, b1 ; load next thread
+        .endm
+
+;;;;;;;;;;;;;;;
+;;; SCHEDULER
+
+;;;;;;;;
+;;;: target contextSwitch: Switch to next thread
+contextSwitch:
+        SAVETHISCT
+        ;; FALL THROUGH
+;;;;;;;;
+;;;: target nextContext: Load next thread and resume it
+nextContext:
+	LOADNEXTCT              ; Pull in next thread
+        jmp CT.sTH.wRES_ADDR    ; and resume it
+                
+
+;;;;;;;;
+;;;: macro SUSPEND_THREAD: Sleep current thread
+;;;  INPUTS: NONE
+;;;  OUTPUTS: NONE
+;;;  NOTES:
+;;;  - Generates an arbitrary delay (of 30ns or more)
+;;;  - Trashes everything but R2, R3.w2, and CT
+SUSPEND_THREAD: .macro
+        jal CT.sTH.wRES_ADDR, contextSwitch
+        .endm
+                
+
+;;; LINUX thread runner 
+LinuxThreadRunner:
+        JAL R3.w2, processPackets ; Surface to C level, check for linux action
+        SUSPEND_THREAD            ; Then context switch
+        JMP LinuxThreadRunner     ; Then try again
+
+;;; Idle thread runner 
+IdleThreadRunner:
+        SUSPEND_THREAD          ; Just context switch
+        JMP IdleThreadRunner    ; Then try again
+
+
+        .text
         .def mainLoop
 mainLoop:
 	.ref processPackets
+l0:     
         JAL R3.w2, processPackets ; Return == 0 if firstPacket done
-	QBNE mainLoop, R14, 0     ; Wait for that before initting state machines
+	QBNE l0, R14, 0 ; Wait for that before initting state machines
 	JAL R3.w2, initStateMachines
 l1:     JAL R3.w2, advanceStateMachines
         JAL R3.w2, processPackets
@@ -91,27 +197,36 @@ addfuncasm:
         ADD R2, R2, 6           ; Pop stack
         JMP r3.w2               ; Return
         
-        
         .def initStateMachines
 initStateMachines:
+        ;; DEMO CODE INIT
 	;; Clear counts
 	ZERO &LiveCounts,STATE_INFO_LEN
-	
         ;; Read initial pin states
 	LOADBIT LiveCounts.RXRDY_STATE, r31, PRUDIR1_RXRDY_R31_BIT  ; pru0 SE, pru1 NW
 	LOADBIT LiveCounts.RXDAT_STATE, r31, PRUDIR1_RXDAT_R31_BIT  ; pru0 SE, pru1 NW
-
-	;; Clear CT
-	ZERO &CT,IOThread_LEN
-	LOOP el1, 3             ; three times around
-	LSL R0.b0, CT.bTHIS_ID, 3 ; R0.b0 = this_id*8
-        XOUT PRUX_SCRATCH, &CT, IOThread_LEN ; Save 'initted' context
-	ADD CT.bTHIS_ID, CT.bTHIS_ID, 1     ; ++this_id
-el1:    
-        ;; Load thread 1
-        LDI R0.b0, 1<<3
-        XIN PRUX_SCRATCH, &CT, IOThread_LEN
-        
+        ;; END DEMO CODE INIT
+	
+	;; Init threads by hand
+        INITTHIS 0*CTRegs, IOThread_LEN, 0, IdleThreadRunner ; Thread ID 0 at shift 0
+        INITNEXT 1*CTRegs, IOThread_LEN            ; Info for thread 1
+        SAVETHISCT                                 ; Stash thread 0
+	
+        INITTHIS 1*CTRegs, IOThread_LEN, 1, IdleThreadRunner ; Thread ID 1 at shift CTRegs
+	INITNEXT 2*CTRegs, IOThread_LEN            ; Info for thread 2
+        SAVETHISCT                                 ; Stash thread 1
+	
+        INITTHIS 2*CTRegs, IOThread_LEN, 2, IdleThreadRunner ; Thread ID 2 at shift 2*CTRegs
+	INITNEXT 3*CTRegs, LinuxThread_LEN         ; Info for thread 3
+        SAVETHISCT                                 ; Stash thread 2
+	
+        INITTHIS 3*CTRegs, LinuxThread_LEN, 3,LinuxThreadRunner ; Thread ID 3 at shift 3*CTRegs
+        INITNEXT 0*CTRegs, IOThread_LEN            ; Next is back to thread 0
+        SAVETHISCT                                 ; Stash thread 3
+        ;; Done with by-hand thread inits
+	
+	LOADNEXTCT              ; Pull thread 0 back in
+	
         SENDVAL PRUX_STR,""" hi from prux_sbst3.asm""", R0.b0 ; Report in
         JMP r3.w2               ; Return
 
@@ -150,7 +265,7 @@ copyOutScratchPad:
         SBBO &CTReg, R2, 0, 4   ; Store first reg of CT on stack
 
         LDI R17,0               ; index = 0
-        MIN R15, R15, 3*IOThread_LEN ; don't read beyond the three threads
+        MIN R15, R15, 4*30      ; can the xfr shift, itself, wrap?
 cosp1:
 	QBGE cosp2, R15, R17     ; Done when idx reaches len
 	LSR R0.b0, R17, 2        ; Get reg of byte: at idx/4
