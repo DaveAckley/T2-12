9ac4712a8b0cb6203217a7b10cebcd977e9ce186
Dave Ackley <ackley@ackleyshack.com>
2017-12-19 00:49:17 -0700

    Reorg asm filenames

---
+++ pru/itcio/firmware/pru0_sbst3.asm
@@ -0,0 +1,7 @@
+        .cdecls
+        %{
+#define ON_PRU 0
+#include "prux.h"
+        %}
+        .copy "prux_sbst3.inc"
+        
---
+++ pru/itcio/firmware/pru1_sbst3.asm
@@ -0,0 +1,7 @@
+        .cdecls
+        %{
+#define ON_PRU 1
+#include "prux.h"
+        %}
+       .copy "prux_sbst3.inc"
+        
