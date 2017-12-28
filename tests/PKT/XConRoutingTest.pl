75b70c32dca800a4e9484a8b466758476d818d74
Dave Ackley <ackley@ackleyshack.com>
2017-12-28 02:02:22 -0700

    Scale-up packet stress tester

--- tests/PKT/XConRoutingTest.pl
+++ tests/PKT/XConRoutingTest.pl
@@ -5,6 +5,8 @@ use lib $FindBin::Bin;
 use Fcntl;
 use Errno qw(EAGAIN);
 
+my $expandos = scalar(@ARGV);
+    
 my $pktdev = "/dev/itc/packets";
 my $mode = O_RDWR;
 my @args = ("-nb",
@@ -21,32 +23,47 @@ if (scalar(@args)) {
     }
 }
 my $gots = 0;
-@args = (@args, @args);
+for (my $i = 0; $i < $expandos; ++$i) {
+    @args = (@args, @args);
+}
+my @sndstoppers = ("\x81END\x81",   "\x82END\x82",   "\x83END\x83",   "\x85END\x85",   "\x86END\x86",   "\x87END\x87");
+my %rcvstoppers = ("\x85END\x81"=>1,"\x86END\x82"=>1,"\x87END\x83"=>1,"\x81END\x85"=>1,"\x82END\x86"=>1,"\x83END\x87"=>1);
+@args = (@args, @sndstoppers);
 use Time::HiRes;
 print "start\n";
 my $start = Time::HiRes::time();
 sysopen(PKTS, $pktdev, $mode) or die "Can't open $pktdev: $!";
 my $pkt;
 my $count;
+my $bytessent = 0;
+my $bytesrcvd = 0;
 while (scalar(@args)) {
     my $pkt = shift @args;
     my $len = syswrite(PKTS, $pkt);
-#    sleep 0.1;
-    # while (defined($count = sysread(PKTS, $pkt, 512))) {
-    #     last if $count == 0;
-    #     print "I GOT($count)='$pkt'\n";
-    # }
+    $bytessent += $len;
+    while (defined($count = sysread(PKTS, $pkt, 512))) {
+        last if $count == 0;
+        $bytesrcvd += $count;
+        delete $rcvstoppers{$pkt};
+    }
 }
-while (defined($count = sysread(PKTS, $pkt, 512))) {
-    last if $count == 0;
-    print ++$gots.":GOT($count)='$pkt'\n";
+while (scalar(keys %rcvstoppers)) {
+    while (defined($count = sysread(PKTS, $pkt, 512))) {
+        last if $count == 0;
+        $bytesrcvd += $count;
+        delete $rcvstoppers{$pkt};
+    }
+#    Time::HiRes::usleep(10);
 }
 my $stop = Time::HiRes::time();
-sleep .25;
-while (defined($count = sysread(PKTS, $pkt, 512))) {
-    last if $count == 0;
-    print ++$gots."FINAL GOTS($count)='$pkt'\n";
-}
 die "Error: $!" unless defined($count) or ($mode & O_NONBLOCK) and $!{EAGAIN};
 close(PKTS) or die "Can't close $pktdev: $!";
-printf("elapsed %f sec\n", $stop-$start);
+my $sec = $stop-$start;
+my $pct = 100.0*$bytesrcvd/$bytessent;
+my $Bps = $bytessent/$sec;
+my $KBps = $Bps/1000.0;
+my $bps = $Bps*8;
+my $Mbps = $bps/1000000.0;
+printf("rcvd %3.0f%% elapsed %f sec; sent %d bytes; %5.2f KBps, %5.3f Mbps\n",
+       $pct,
+       $sec, $bytessent, $KBps, $Mbps);
