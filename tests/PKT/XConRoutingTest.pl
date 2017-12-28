350aa4b8cb2bd50054cd7fa8e81280457e6e5bb7
Dave Ackley <ackley@ackleyshack.com>
2017-12-28 00:16:53 -0700

    Run more standalone and measure more

--- tests/PKT/XConRoutingTest.pl
+++ tests/PKT/XConRoutingTest.pl
@@ -7,31 +7,46 @@ use Errno qw(EAGAIN);
 
 my $pktdev = "/dev/itc/packets";
 my $mode = O_RDWR;
-if (scalar(@ARGV)) {
-    if ($ARGV[0] eq "-nb") {
-        shift @ARGV;
+my @args = ("-nb",
+            "\x81GO1NE girl grill chief complaintive been w2orking on the rr all the live long day",
+            "\x82GO2ET phone home run down town bus run all night long doo dah doo dah dit dit dit dah",
+            "\x83GO3SE spot run down home base belong to us three us four us five cents a dance",
+            "\x85GO5SW atting flies firing the odd shot here and there at the fair get some flare doo dah",
+            "\x86GO6WTabix white bread and call it wheat but society survives the apocalypto carribean",
+            "\x87GO7NW passage rosemary thyme bomb thumb bluze brothers mothers of invention pretension");
+if (scalar(@args)) {
+    if ($args[0] eq "-nb") {
+        shift @args;
         $mode |= O_NONBLOCK;
     }
 }
+my $gots = 0;
+@args = (@args, @args);
+use Time::HiRes;
+print "start\n";
+my $start = Time::HiRes::time();
 sysopen(PKTS, $pktdev, $mode) or die "Can't open $pktdev: $!";
 my $pkt;
 my $count;
-while (scalar(@ARGV)) {
-    my $pkt = shift @ARGV;
+while (scalar(@args)) {
+    my $pkt = shift @args;
     my $len = syswrite(PKTS, $pkt);
-    while (defined($count = sysread(PKTS, $pkt, 512))) {
-        last if $count == 0;
-        print "I GOT($count)='$pkt'\n";
-    }
+#    sleep 0.1;
+    # while (defined($count = sysread(PKTS, $pkt, 512))) {
+    #     last if $count == 0;
+    #     print "I GOT($count)='$pkt'\n";
+    # }
 }
 while (defined($count = sysread(PKTS, $pkt, 512))) {
     last if $count == 0;
-    print "I GOT($count)='$pkt'\n";
+    print ++$gots.":GOT($count)='$pkt'\n";
 }
-sleep 1.5;
+my $stop = Time::HiRes::time();
+sleep .25;
 while (defined($count = sysread(PKTS, $pkt, 512))) {
     last if $count == 0;
-    print "FINAL GOTS($count)='$pkt'\n";
+    print ++$gots."FINAL GOTS($count)='$pkt'\n";
 }
 die "Error: $!" unless defined($count) or ($mode & O_NONBLOCK) and $!{EAGAIN};
 close(PKTS) or die "Can't close $pktdev: $!";
+printf("elapsed %f sec\n", $stop-$start);
