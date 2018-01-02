20a908c2af320daea265ca80177719bb62ffbe17
Dave Ackley <ackley@ackleyshack.com>
2018-01-02 14:20:44 -0700

    Flesh out SharedState; add SharedStateSelector

--- pru/itcio/common/SharedState.c
+++ pru/itcio/common/SharedState.c
@@ -1,8 +1,39 @@
 #include "SharedState.h"
 
+void initSharedStateSelector(struct SharedStateSelector * sss)
+{
+  initSharedStateSelectorInline(sss);
+}
+
+struct PacketBuffer * getPacketBufferIfAny(struct SharedState * ss, struct SharedStateSelector * sss)
+{
+  return getPacketBufferIfAnyInline(ss, sss);
+}
+
 void initSharedState(struct SharedState * ss)
 {
-  return initSharedStateInline(ss);
+  unsigned i;
+  for (i = 0; i < 2; ++i)
+    initSharedStatePerPru(&ss->pruState[i]);
+}
+
+void initQoSPacketBufferPair(struct QoSPacketBufferPair * qpbp)
+{
+  pbInit((struct PacketBuffer*) &qpbp->fast, RING_BUFFER_BITS_MED);
+  pbInit((struct PacketBuffer*) &qpbp->slow, RING_BUFFER_BITS_MED);
+}
+
+void initSharedStatePerITC(struct SharedStatePerITC * sspi)
+{
+  initQoSPacketBufferPair(&sspi->outbound);
+  initQoSPacketBufferPair(&sspi->inbound);
+}
+
+void initSharedStatePerPru(struct SharedStatePerPru * sspp)
+{
+  unsigned i;
+  for (i = 0; i < 3; ++i)
+    initSharedStatePerITC(&sspp->pruDirState[i]);
 }
 
 struct SharedState * getSharedStatePhysical()
--- pru/itcio/common/SharedState.h
+++ pru/itcio/common/SharedState.h
@@ -25,6 +25,7 @@ struct QoSPacketBufferPair {
   PacketBufferStorageMED fast;  /* Priority rate queue */
   PacketBufferStorageMED slow;  /* Bulk rate queue */
 };
+extern void initQoSPacketBufferPair(struct QoSPacketBufferPair * qpbp) ;
 
 static inline struct PacketBuffer * getPacketBufferForQoSInline(struct QoSPacketBufferPair * qpbp, unsigned bulk) {
   return
@@ -55,6 +56,7 @@ struct SharedStatePerITC {
   struct QoSPacketBufferPair outbound;  /* Packets outbound from host to ITC and beyond */
   struct QoSPacketBufferPair inbound;   /* Packets inbound to host from ITC and beyond */
 };
+extern void initSharedStatePerITC(struct SharedStatePerITC * sspi) ;
 
 static inline struct PacketBuffer * sspiNextOutboundInline(struct SharedStatePerITC * sspi) {
   return getNextPacketBufferToReadInline(&sspi->outbound);
@@ -72,7 +74,7 @@ extern struct PacketBuffer * sspiNextInbound(struct SharedStatePerITC * sspi) ;
 struct SharedStatePerPru {
   struct SharedStatePerITC pruDirState[3];
 };
-
+extern void initSharedStatePerPru(struct SharedStatePerPru * sspp) ;
 
 ////////////////////
 struct SharedState {
@@ -92,10 +94,40 @@ static inline struct SharedState * getSharedStateVirtualInline() {
   return (struct SharedState*) sharedStateVirtualBaseAddress;
 }
 
-static inline void initSharedStateInline(struct SharedState * ss) {
-  memset(ss, 0, sizeof(*ss));
+extern void initSharedState(struct SharedState * ss) ;
+
+////////////////////
+struct SharedStateSelector {
+  unsigned char pru;
+  unsigned char prudir;
+  unsigned char inbound;
+  signed char bulk;
+};
+
+static inline void initSharedStateSelectorInline(struct SharedStateSelector * sss)
+{
+  memset(sss, 0, sizeof(*sss));
 }
 
-extern void initSharedState(struct SharedState * ss) ;
+extern void  initSharedStateSelector(struct SharedStateSelector * sss) ;
+
+static inline struct PacketBuffer * getPacketBufferIfAnyInline(struct SharedState * ss, struct SharedStateSelector * sss)
+{
+  struct SharedStatePerPru * sspp;
+  struct SharedStatePerITC * sspi;
+  struct QoSPacketBufferPair * qpbp;
+  struct PacketBuffer * pb;
+  if (!ss || !sss || sss->pru > 1 || sss->prudir > 2) return 0;
+  sspp = &ss->pruState[sss->pru];
+  sspi = &sspp->pruDirState[sss->prudir];
+  qpbp = sss->inbound ? &sspi->inbound : &sspi->outbound;
+  if (sss->bulk < 0) 
+    pb = getNextPacketBufferToRead(qpbp);        
+  else
+    pb = getPacketBufferForQoS(qpbp, sss->bulk);
+  return pb;
+}
+
+extern struct PacketBuffer * getPacketBufferIfAny(struct SharedState * ss, struct SharedStateSelector * sss) ;
 
 #endif /* SHAREDSTATE_H */
