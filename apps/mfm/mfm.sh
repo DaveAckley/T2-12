d23a7c0285467965776a49b68f82fe316084a52c
Dave Ackley <ackley@ackleyshack.com>
2018-01-04 02:28:22 -0700

    More failing flailing on touchscreen calibration

--- apps/mfm/mfm.sh
+++ apps/mfm/mfm.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Thu Jan  4 01:46:54 2018 The struggle to fix the touchscreen flares
+# up once again, but again, so far, with no satisfying resolution
+export SDL_MOUSEDRV=TSLIB
+export SDL_MOUSEDEV=/dev/input/touchscreen
+export SDL_FBDEV=/dev/fb0
+export SDL_VIDEODRIVER=fbcon
+
 mfms {1C1} --run --sf --sw 480 --sh 320 -a 0 --start-file /usr/lib/ulam/MFM/res/mfs/T2-12-13-start.mfs &
 export MFM_PID=$!
 echo "Started $MFM_PID"
