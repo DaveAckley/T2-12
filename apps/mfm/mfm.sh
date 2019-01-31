668e4fb18430fd3d66a6f40992bb3980f7a8c994
Dave Ackley <ackley@ackleyshack.com>
2019-01-30 22:56:28 -0700

    Capture temp args for new 1H1 geometry

--- apps/mfm/mfm.sh
+++ apps/mfm/mfm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#OFFICIAL Mon Jan 21 09:45:23 2019:
 mfms {1C1} --run --sf --sw 480 --sh 320 -a 0 --start-file /usr/lib/ulam/MFM/res/mfs/T2-12-13-start.mfs &
+#MODRUN COOL NEW IF YOU HAVE /home/t2/MFMTMP:
+#/home/t2/GITHUB/MFM/bin/mfms {1H1} --run -n -a 0 --sw 480 --sh 320 --sf -ep /home/t2/MFMTMP/.gen/bin/libcue.so --start-file /home/t2/MFMTMP/10.mfs
 export MFM_PID=$!
 echo "Started $MFM_PID"
 sleep 1
