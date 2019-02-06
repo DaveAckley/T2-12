63d99fa0f988165850954741a9075c422f61780d
Dave Ackley <ackley@ackleyshack.com>
2019-02-06 14:54:14 -0700

    Cut at C type and D type packets

--- apps/t2/cdm/cdm.pl
+++ apps/t2/cdm/cdm.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w  # -*- perl -*-
 use Fcntl;
-use File::Path qw(make_path);
+use File::Path qw(make_path remove_tree);
 use Errno qw(EAGAIN);
 use Time::HiRes;
 use List::Util qw/shuffle/;
@@ -65,6 +65,13 @@ my $pendingSubdir = "pending";
 my @subDirs = ($commonSubdir, $pendingSubdir);
 my $commonPath = "$baseDir/$commonSubdir";
 my $pendingPath = "$baseDir/pending";
+
+sub flushPendingDir {
+    my $count = remove_tree($pendingPath);
+    if ($count > 1) {
+        print "Flushed $count $pendingPath files\n";
+    }
+}
 
 sub checkInitDir {
     my $dir = shift;
@@ -139,6 +146,14 @@ sub checksumWholeFile {
 }
 
 my $globalCheckedFilesCount = 0;
+my %seqnoMap;
+
+sub assignSeqnoForFilename {
+    my $filename = shift;
+    my $seqno = ++$globalCheckedFilesCount;
+    $seqnoMap{$seqno} = $filename;
+    return $seqno;
+}
 
 sub getFinfoPath {
     my $finfo = shift;
@@ -177,30 +192,118 @@ sub checkMFZDataFor {
     $finfo->{innerTimestamp} = $epoch;
     $finfo->{currentLength} = $finfo->{length};
     $finfo->{checkedLength} = $finfo->{length};
-    $finfo->{seqno} = ++$globalCheckedFilesCount;
+    $finfo->{seqno} = assignSeqnoForFilename();
     return 1;
+}
+
+sub checkAndReleasePendingFile {
+    my $finfo = shift;
+    die "GOT THERE $finfo";
+}
+
+sub lexDecode {
+    my $lex = shift;
+    if ($lex =~ s/^9//) {
+        my ($len,$rest) = lexDecode($lex);
+        my $num = substr($rest,0,$len);
+        substr($rest,0,$len) = "";
+        return ($num,$rest);
+    } elsif ($lex =~ s/^[0-8]//) {
+        my $len = $1;
+        my $num = substr($lex,0,$len);
+        substr($lex,0,$len) = "";
+        return ($num,$lex);
+    } else {
+        return undef;
+    }
+}
+
+sub lexEncode {
+    my $num = shift;
+    my $len = length($num);
+    return $len.$num if $len < 9;
+    return "9".lexEncode($len).$num;
+}
+
+sub generateSKU {
+    my ($finfo, $seqno) = @_;
+    my $sku = sprintf("%s%02x%02x%03d%s",
+                      substr($finfo->{filename},0,1),
+                      ord(substr($finfo->{checksum},0,1)),
+                      ord(substr($finfo->{checksum},1,1)),
+                      $finfo->{innerTimestamp}%1000,
+                      lexEncode($seqno));
+    print "SKU($sku)\n";
+    return $sku;
+}
+
+sub checkSKU {
+    my $sku = shift;
+    $sku =~ /^(.)([0-9a-fA-F]{2})([0-9a-fA-F]{2})(\d\d\d)(.+)$/
+        or return undef;
+    my ($fnchar,$cs0,$cs1,$bottim,$lexsi) = ($1,hex($2),hex($3),$4,$5);
+    my $seqno = lexDecode($lexsi);
+    defined $seqno or return undef;
+
+    my $filename = $seqnoMap{$seqno};
+    defined $filename or return undef;
+    substr($filename,0,1) eq $fnchar or return undef;
+    ord(substr($finfo->{checksum},0,1)) == $cs0 or return undef;
+    ord(substr($finfo->{checksum},1,1)) == $cs1 or return undef;
+    $finfo->{innerTimestamp}%1000 == $bottim or return undef;
+
+    return $finfo;
+}
+
+sub checkPendingFile {
+    # Pick a random pending
+    my $pref = getSubdirModel($pendingSubdir);
+    my @fnames = keys %{$pref};
+    my $count = scalar @fnames;
+    return unless $count > 0;
+
+    my $filename = $fnames[createInt($count)];
+    my $finfo = $pref->{$filename};
+    my $len = $finfo->{length};
+    my $cur = $finfo->{currentLength};
+    if ($len == $cur) {
+        checkAndReleasePendingFile($finfo);
+        return;
+    }
+    if ($finfo->{timeCount} > 0) {
+        --$finfo->{timeCount};
+        return;
+    }
+    my ($dir,$seqno) = selectProvider($finfo);
+    if (!defined($dir) || !defined($seqno)) {
+        print "No provider found for $filename in $finfo?\n";
+        return;
+    }
+    my $sku = generateSKU($finfo,$seqno);
+
+    my $contentRequestCode = "C";
+    my $pkt = chr(0x80+$dir).chr($CDM_PKT_TYPE).$contentRequestCode;
+    $pkt = addLenArgTo($pkt,$sku);
+    $pkt = addLenArgTo($pkt,$cur);
+    $finfo->{timeCount} = 1;  # don't spam requests too fast
+    print STDERR "REQUEST($pkt)\n";
+    writePacket($pkt);
 }
 
 sub checkCommonFile {
     if (scalar(@pathsToLoad) == 0) {
         loadCommonFiles();
-        return;
+        return 1; # did work
     }
     my $filename = shift @pathsToLoad;
     return unless defined $filename && $filename =~ /[.]mfz$/;
     if (length($filename) > $MAX_MFZ_NAME_LENGTH) {  #### XXX WOAH
         print "MFZ filename too long '$filename'\n";
-        return;
+        return 1;
     }
 
     my $finfo = getFinfoFromCommon($filename);
     my $path = getFinfoPath($finfo) || die "No path";
-
-    # Check if file is incomplete
-    if ($finfo->{currentLength} < $finfo->{length}) {
-        requestFileChunk($finfo);
-        return; # and that's all we do for now.
-    }
 
     # Check if modtime change
     my $modtime = -M $path;
@@ -214,18 +317,21 @@ sub checkCommonFile {
     # Need checksum?
     if (!defined $finfo->{checksum}) {
         $finfo->{checksum} = checksumWholeFile($path);
-        return; # That's enough for now..
+        return 1; # That's enough for now..
     }
 
     # Need MFZ info?
     if (checkMFZDataFor($finfo)) {
-        return; # That's plenty for now..
+        return 1; # That's plenty for now..
     }
 
     # This file is ready.  Maybe announce it to somebody?
     my $aliveNgb = getRandomAliveNgb();
-    return unless defined $aliveNgb && oneIn(3);
-    announceFileTo($aliveNgb,$finfo);
+    if (defined $aliveNgb && oneIn(3)) {
+        announceFileTo($aliveNgb,$finfo);
+        return 1;
+    }
+    return 0; # didn't do any 'real' work..
 }
 
 # For files we have locally and completely
@@ -255,8 +361,35 @@ sub newFinfoBare {
         seqno => undef,
         currentLength => undef,
         checkedLength => 0,
-        otherSeqnos => [],
+        timeCount => 0,
+        otherSeqnos =>[],
     };
+}
+
+sub selectProvider {
+    my $finfo = shift;
+    my $sdir;
+    my $sseq;
+    my $count = 0;
+    for (my $dir = 0; $dir < 8; ++$dir) {
+        my $seq = $finfo->{otherSeqnos}->[$dir];
+        if (defined($seq) && oneIn(++$count)) {
+            $sdir = $dir;
+            $sseq = $seq;
+        }
+    }
+    return ($sdir,$sseq);
+}
+
+sub refreshProvider {
+    my ($finfo,$dir,$seq) = @_;
+    my $ageDir = randDir();
+    # Age out one dir (might be empty)
+    $finfo->{otherSeqnos}->[$ageDir] = undef;
+
+    # Refresh us
+    $finfo->{otherSeqnos}->[$dir] = $seq;
+    print "FRESH $dir $finfo ".$finfo->{filename}." ".$finfo->{otherSeqnos}->[$dir]."\n";
 }
 
 sub getFinfoFromCommon {
@@ -319,6 +452,13 @@ sub oddsOf {
     return rand($outof) < $this;
 }
 
+sub createInt {
+    my $max = shift;
+    my $imax = int($max);
+    die if $imax < 1;
+    return int(rand($imax));
+}
+
 sub oneIn {
     return oddsOf(1, shift);
 }
@@ -368,7 +508,10 @@ sub doBackgroundWork {
     }
 
     # COMMON MGMT
-    checkCommonFile();
+    return if checkCommonFile();
+
+    # PENDING MGMT
+    return if checkPendingFile();
 }
 
 sub announceFileTo {
@@ -417,7 +560,7 @@ sub checkAnnouncedFile {
         $pfinfo->{innerTimestamp} = $timestamp;
         $pfinfo->{currentLength} = 0;
         $pfinfo->{checkedLength} = 0;
-        $pfinfo->{otherSeqnos}->[$dir] = $seqno;
+        refreshProvider($pfinfo,$dir,$seqno);
         touchFile("$pendingPath/$filename");
 
         $pendingref->{$filename} = $pfinfo;
@@ -426,7 +569,7 @@ sub checkAnnouncedFile {
 
     ## Matched in pending: Refresh sku
     if (!defined($finfo) && defined($pfinfo)) {
-        $pfinfo->{otherSeqnos}->[$dir] = $seqno;
+        refreshProvider($pfinfo,$dir,$seqno);
         return;
     }
 
@@ -466,6 +609,25 @@ sub processFileAnnouncement {
     checkAnnouncedFile($filename,$contentLength,$checksum,$timestamp,$seqno,$dir);
 }
 
+sub processChunkRequest {
+    my @bytes = @_;
+    my $bref = \@bytes;
+    my $dir = ord($bytes[0])&0x7;
+    # [0:2] known to be CDM C type
+    my $lenPos = 3;
+    my ($sku, $startingIndex);
+    ($sku,$lenPos) = getLenArgFrom($lenPos,$bref);
+    ($startingIndex,$lenPos) = getLenArgFrom($lenPos,$bref);
+    my $finfo = checkSKU($sku);
+    if (!defined($finfo)) {
+        print "Bad SKU $sku\n";
+        return;
+    }
+    my $filename = $finfo->{filename};
+    print "CR(sku=$sku,fn=$filename,si=$startingIndex)\n";
+    # XXXX DO ME DO ME
+}
+
 sub getSKUForDir {
     my ($finfo,$dir) = @_;
 
@@ -491,6 +653,10 @@ sub processPacket {
         }
         if ($bytes[2] eq "F") {
             processFileAnnouncement(@bytes);
+            return;
+        }
+        if ($bytes[2] eq "C") {
+            processChunkRequest(@bytes);
             return;
         }
     }
@@ -526,6 +692,7 @@ sub eventLoop {
 }
 
 sub main {
+    flushPendingDir();
     checkInitDirs();
     openPackets();
     eventLoop();
