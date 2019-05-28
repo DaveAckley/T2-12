5aa0e29b51a35e98c6f01572f59c1f35fb40c778
Dave Ackley <ackley@ackleyshack.com>
2019-05-28 00:02:39 -0600

    Restart cdm.pl when it dies

--- apps/cdm/cdm.sh
+++ apps/cdm/cdm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-TERM=dumb /home/t2/T2-12/apps/cdm/cdm.pl
+while [ true ] ; do
+    TERM=dumb /home/t2/T2-12/apps/cdm/cdm.pl
+    echo cdm exited status $? -- RESTARTING
+    sleep 2
+done
 
 
 
