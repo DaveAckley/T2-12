0f44f1358553383bb1751eef1f5f8054187b21c3
root <root@beaglebone.localdomain>
2019-12-12 02:05:47 -0700

    Boot sequence redo for 9-11

--- files/opt/t2-startup.sh
+++ files/opt/t2-startup.sh
@@ -2,35 +2,35 @@
 
 # Run final boot time configurations for T2 pins
 
-logger STARTING T2 FINAL BOOT CONFIG
+logger STARTING T2 FINAL BOOT CONFIG -- NOT REALLY THO
 
 logger T2 FINAL BOOT CONFIG: cape-universal2
-echo cape-universal2 > /sys/devices/platform/bone_capemgr/slots
-sleep 2
+#echo cape-universal2 > /sys/devices/platform/bone_capemgr/slots
+#sleep 2
 
 logger T2 FINAL BOOT CONFIG: boot-config-pin
-config-pin -f /opt/scripts/t2/boot-config-pin.txt
-sleep 1
+#config-pin -f /opt/scripts/t2/boot-config-pin.txt
+#sleep 1
 
 logger T2 FINAL BOOT CONFIG: ADC
-echo T2-ADC > /sys/devices/platform/bone_capemgr/slots
-sleep 1
+#echo T2-ADC > /sys/devices/platform/bone_capemgr/slots
+#sleep 1
 
 logger T2 FINAL BOOT CONFIG: waveshare35a
-echo waveshare35a > /sys/devices/platform/bone_capemgr/slots
+#echo waveshare35a > /sys/devices/platform/bone_capemgr/slots
 
 logger T2 FINAL BOOT CONFIG: PRUs
 #echo EXCEPT NOT BECAUSE THEY ARE ALREADY OK? 
 echo "Rebooting PRUs"
-echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/unbind 2>/dev/null
-echo "4a338000.pru1"  > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
-sleep 1
-echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/bind
-echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/bind
+#echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/unbind 2>/dev/null
+#echo "4a338000.pru1"  > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
+#sleep 1
+#echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/bind
+#echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/bind
 
 logger T2 FINAL BOOT CONFIG: CPUFREQ
 echo "Defaulting to 720MHz for heat management"
-cpufreq-set -f 720MHz
+#cpufreq-set -f 720MHz
 
 logger T2 FINAL BOOT CONFIG FINISHED
 
