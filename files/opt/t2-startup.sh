ac4cb3ab77587f8ff5c99239e7b8d79a3795f925
Dave Ackley <ackley@ackleyshack.com>
2019-12-15 04:49:58 -0700

    More3 incompetent superstitious systemd tweaking

--- files/opt/t2-startup.sh
+++ files/opt/t2-startup.sh
@@ -2,24 +2,24 @@
 
 # Run final boot time configurations for T2 pins
 
-logger STARTING T2 FINAL BOOT CONFIG -- NOT REALLY THO
+logger STARTING T2 FINAL BOOT CONFIG -- SORT OF
 
-logger T2 FINAL BOOT CONFIG: cape-universal2
+#logger T2 FINAL BOOT CONFIG: cape-universal2
 #echo cape-universal2 > /sys/devices/platform/bone_capemgr/slots
 #sleep 2
 
 logger T2 FINAL BOOT CONFIG: boot-config-pin
-#config-pin -f /opt/scripts/t2/boot-config-pin.txt
+config-pin -f /opt/scripts/t2/boot-config-pin.txt
 #sleep 1
 
-logger T2 FINAL BOOT CONFIG: ADC
+#logger T2 FINAL BOOT CONFIG: ADC
 #echo T2-ADC > /sys/devices/platform/bone_capemgr/slots
 #sleep 1
 
-logger T2 FINAL BOOT CONFIG: waveshare35a
+#logger T2 FINAL BOOT CONFIG: waveshare35a
 #echo waveshare35a > /sys/devices/platform/bone_capemgr/slots
 
-logger T2 FINAL BOOT CONFIG: PRUs
+#logger T2 FINAL BOOT CONFIG: PRUs
 #echo EXCEPT NOT BECAUSE THEY ARE ALREADY OK? 
 echo "Rebooting PRUs"
 #echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/unbind 2>/dev/null
@@ -28,8 +28,8 @@ echo "Rebooting PRUs"
 #echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/bind
 #echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/bind
 
-logger T2 FINAL BOOT CONFIG: CPUFREQ
-echo "Defaulting to 720MHz for heat management"
+#logger T2 FINAL BOOT CONFIG: CPUFREQ
+#echo "Defaulting to 720MHz for heat management"
 #cpufreq-set -f 720MHz
 
 logger T2 FINAL BOOT CONFIG FINISHED
