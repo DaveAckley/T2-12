3d3ab2ae69a6bf57d1522d3847f69aabeea1f45d
Dave Ackley <ackley@livingcomputation.org>
2021-02-03 16:09:48 -0700

    Create mfm.pl

--- apps/mfm/mfm.sh
+++ apps/mfm/mfm.sh
@@ -8,7 +8,13 @@
 cd /
 while [ true ] ; do
     echo Launching mfmt2
-    /home/t2/MFM/bin/mfmt2 -w /home/t2/MFM/res/mfmt2/wconfig.txt -z MFMT2-FAKE-MFZID -t
+#    /home/t2/MFM/bin/mfmt2 -w /home/t2/MFM/res/mfmt2/wconfig.txt -z MFMT2-FAKE-MFZID -t
+# Tue Jan 19 07:14:13 2021 We'll just let this keep dying until cdmss-a1-51f131.mfz is installed??
+# Thu Jan 21 04:33:56 2021 Is -t killing the AER??
+#    /home/t2/MFM/bin/mfmt2 -w /home/t2/MFM/res/mfmt2/wconfig.txt -z cdmss-a1-51f131.mfz -t/tmp -e/home/t2/physics/a1/code/.gen/bin/libcue.so
+#    /home/t2/MFM/bin/mfmt2 -w /home/t2/MFM/res/mfmt2/wconfig.txt -z cdmss-a1-51f131.mfz -e/home/t2/physics/a1/code/.gen/bin/libcue.so
+    # Just dish to mfm.pl to do config-specific launching
+    /home/t2/T2-12/apps/mfm/mfm.pl
     echo mfmt2 exited status $? -- RESTARTING
 #    echo Launching mfmt2
 #    /home/t2/T2-12/apps/mfm/RUN_SDL /home/t2/MFM/bin/mfmt2 {{1H1}} --sw 480 --sh 320 --sf --start-file /home/t2/T2-12/apps/mfm/14stats.mfs --startsymbol SD -ep /home/t2/T2-12/apps/mfm/libcueSD.so --run -a 0 -n -wf 10 -e 10
