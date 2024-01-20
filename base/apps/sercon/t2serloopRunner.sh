a8a3eefe9fafa6340785caee37554bc6a35e1dc0
Dave Ackley <ackley@livingcomputation.org>
2024-01-20 16:26:47 -0700

    Relativize path

--- base/apps/sercon/t2serloopRunner.sh
+++ base/apps/sercon/t2serloopRunner.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export MYPYSERIAL=/home/t2/T2-12/base/files/misc/root/.local/lib/python3.7/site-packages/
 export PYTHONPATH=$PYTHONPATH:$MYPYSERIAL
 
 while [ 1 ] ; do
     date '+STARTING T2 SERLOOP %Y-%m-%d %H:%M:%S'
-    ./t2serloop.py
+    $SCRIPT_DIR/t2serloop.py
     sleep 1
 done
